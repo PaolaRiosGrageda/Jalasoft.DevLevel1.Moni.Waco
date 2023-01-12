001fc3f828dddde26b19bcd7d940d3f8380b41d2
Israel Casanova <Isrrael.Casanova@jala.university>
2023-01-11 23:43:19 -0400

    First Test

---
+++ TestWaco/UnitTest1.cs
@@ -0,0 +1,27 @@
+using WACO;
+
+namespace TestWaco
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+
+        [TestMethod]
+        public void TestValidateUserCreationWithDuplicateValues()
+        {
+            User myUser1 = new User(345431, "Pedro", "Vargas");
+            User myUser2 = new User(123412, "Pablo", "Camacho");
+            User myUser3 = new User(544323, "Jorge", "Venegas");
+
+            WacoController myList = new WacoController();
+            myList.Add(myUser1);
+            myList.Add(myUser2);
+            myList.Add(myUser3);
+
+            bool status = myList.VerifyCI(544323);
+            Assert.AreEqual(status, true);
+        }
+    }
+
+}
---
+++ WACO/WacoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WACO
+{
+    public class WacoController
+    {
+        public List<User> _users = new List<User>();
+
+        public void Add(User myUser)
+        {
+            _users.Add(myUser);
+        }
+        public bool VerifyCI(int ci)
+        {
+            bool status = false;
+            foreach (var item in _users)
+            {
+                if (item.CI == ci)
+                {
+                    status = true;
+                }
+            }
+            return status;
+        }
+    }
+}
